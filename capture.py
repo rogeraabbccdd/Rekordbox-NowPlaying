ea40c8474fdebd75cb2eac1401c76d31a05de420
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-01-18 13:18:08 +0800

    Process image to Improve tesseract quality

--- capture.py
+++ capture.py
@@ -1,7 +1,7 @@
 # ***************
 # Imports
 # ***************
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageChops
 from config import tesseract_cmd, captureInterval, lang
 import numpy as np
 import win32gui
@@ -26,6 +26,9 @@ def capture ():
   print('Capturing...')
   global data
 
+  sharpness = 5.0
+  contrast = 5
+
   # Find rekordbox window
   hwnd = win32gui.FindWindow(None, 'rekordbox')
   # Find rekordbox window position
@@ -33,6 +36,10 @@ def capture ():
 
   # Find master track by getting track 1 master text color
   im = ImageGrab.grab(bbox=(862, 318, 863, 319))
+  enh_sha = ImageEnhance.Sharpness(im)
+  im = enh_sha.enhance(sharpness)
+  enh_con = ImageEnhance.Contrast(im)
+  im = enh_con.enhance(contrast)
   im_matrix = np.array(im)
   Track = 2 if im_matrix[0][0][0] == 19 else 1
 
@@ -40,47 +47,62 @@ def capture ():
     # Get Track 1 title text position
     T1TitleLeft = left + 69
     T1TitleTop = top + 300
-    T1TitleWidth = 735
-    T1TitleHeight = 23
+    T1TitleWidth = 732
+    T1TitleHeight = 22
     T1TitleRight = T1TitleLeft + T1TitleWidth
     T1TitleBot = T1TitleTop + T1TitleHeight
     # Take screenshot of track 1 title
     im = ImageGrab.grab(bbox=(T1TitleLeft, T1TitleTop, T1TitleRight, T1TitleBot))
+    enh_sha = ImageEnhance.Sharpness(im)
+    im = enh_sha.enhance(sharpness)
+    enh_con = ImageEnhance.Contrast(im)
+    im = enh_con.enhance(contrast)
     # Save screenshot
     # im.save('./T1Title.png')
+    # im.show()
     # Extract text
-    title = pytesseract.image_to_string(im, lang=lang, config='--psm 13').strip()
+    title = pytesseract.image_to_string(im, lang=lang, config='--psm 7 --oem 1').strip()
     # print(title)
 
     # Get Track 1 composer text position
     T1ComposerLeft = left + 69
     T1ComposerTop = top + 322
-    T1ComposerWidth = 154
+    T1ComposerWidth = 153
     T1ComposerHeight = 15
     T1ComposerRight = T1ComposerLeft + T1ComposerWidth
     T1ComposerBot = T1ComposerTop + T1ComposerHeight
     # Take screenshot of track 1 composer
     im = ImageGrab.grab(bbox=(T1ComposerLeft, T1ComposerTop, T1ComposerRight, T1ComposerBot))
+    enh_sha = ImageEnhance.Sharpness(im)
+    im = enh_sha.enhance(sharpness)
+    enh_con = ImageEnhance.Contrast(im)
+    im = enh_con.enhance(contrast)
     # Save screenshot
     # im.save('./T1Composer.png')
+    # im.show()
     # Extract text
-    composer = pytesseract.image_to_string(im, lang=lang, config='--psm 13').strip()
+    composer = pytesseract.image_to_string(im, lang=lang, config='--psm 7 --oem 1').strip()
     # print(composer)
 
   else:
     # Get Track 2 title text position
     T2TitleLeft = left + 1077
     T2TitleTop = top + 300
-    T2TitleWidth = 735
-    T2TitleHeight = 23
+    T2TitleWidth = 732
+    T2TitleHeight = 22
     T2TitleRight = T2TitleLeft + T2TitleWidth
     T2TitleBot = T2TitleTop + T2TitleHeight
     # Take screenshot of track 1 title
     im = ImageGrab.grab(bbox=(T2TitleLeft, T2TitleTop, T2TitleRight, T2TitleBot))
+    enh_sha = ImageEnhance.Sharpness(im)
+    im = enh_sha.enhance(sharpness)
+    enh_con = ImageEnhance.Contrast(im)
+    im = enh_con.enhance(contrast)
     # Save screenshot
     # im.save('./T2Title.png')
+    # im.show()
     # Extract text
-    title = pytesseract.image_to_string(im, lang=lang, config='--psm 13').strip()
+    title = pytesseract.image_to_string(im, lang=lang, config='--psm 7 --oem 1').strip()
     # print(title)
 
     # Get Track 2 composer text position
@@ -92,10 +114,15 @@ def capture ():
     T2ComposerBot = T2ComposerTop + T2ComposerHeight
     # Take screenshot of track 1 composer
     im = ImageGrab.grab(bbox=(T2ComposerLeft, T2ComposerTop, T2ComposerRight, T2ComposerBot))
+    enh_sha = ImageEnhance.Sharpness(im)
+    im = enh_sha.enhance(sharpness)
+    enh_con = ImageEnhance.Contrast(im)
+    im = enh_con.enhance(contrast)
     # Save screenshot
     # im.save('./T2Composer.png')
+    # im.show()
     # Extract text
-    composer = pytesseract.image_to_string(im, lang=lang, config='--psm 13').strip()
+    composer = pytesseract.image_to_string(im, lang=lang, config='--psm 7 --oem 1').strip()
     # print(composer)
 
   # Update global variables
